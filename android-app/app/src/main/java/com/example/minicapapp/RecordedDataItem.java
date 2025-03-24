734aa2cde0ebd8ef1683d4be193722fb8b74e2b5
pascalypperciel <pypperciel@gmail.com>
2025-03-24 17:37:12 -0400

    Recycler view now works but is ugly

--- android-app/app/src/main/java/com/example/minicapapp/RecordedDataItem.java
+++ android-app/app/src/main/java/com/example/minicapapp/RecordedDataItem.java
@@ -4,14 +4,12 @@ public class RecordedDataItem {
     private long sessionID; // The sessionID associated with the subject's profile.
     private String sessionName; // The test's name.
     private String sessionTimestamp; // The test's date.
-    private String testType; // The type of test conducted (e.g., Compressive, Tensile, etc.)
     private float initialLength; // The initial length of the test sample
     private float initialArea; // The initial cross-sectional area of the test sample
 
-    public RecordedDataItem(long sessionID, String sessionName, String testType, float initialLength, float initialArea) {
+    public RecordedDataItem(long sessionID, String sessionName, float initialLength, float initialArea) {
         this.sessionID = sessionID;
         this.sessionName = sessionName;
-        this.testType = testType;
         this.initialLength = initialLength;
         this.initialArea = initialArea;
     }
@@ -38,14 +36,6 @@ public class RecordedDataItem {
 
     public void setSessionTimestamp(String sessionTimestamp) {
         this.sessionTimestamp = sessionTimestamp;
-    }
-
-    public String getTestType() {
-        return testType;
-    }
-
-    public void setTestType(String testType) {
-        this.testType = testType;
     }
 
     public float getInitialLength() {
