4ec4a8cf6160aec207d332d597d6f65edd924198
Jessica Socher <jessica.socher@mail.concordia.ca>
2025-04-04 16:48:47 -0400

    added true stress strain graph

--- backend/routes/graphs_routes.py
+++ backend/routes/graphs_routes.py
@@ -115,7 +115,7 @@ def find_best_interval(x_data, y_data):
 
     return best_interval, best_m, best_b
     
-#create the 4 different graphs
+#create the 5 different graphs
 
 def create_graphs(distances, weights, temperatures, timestamps, session_id, initial_length, initial_area, offset):
     graphs_list = []
@@ -128,11 +128,87 @@ def create_graphs(distances, weights, temperatures, timestamps, session_id, init
     avg_temperature = np.mean(temperatures)
     time_seconds = [(ts - timestamps[0]).total_seconds() for ts in timestamps]
 
+    # --- Engr Stress vs Strain Plot ---
+
+    #get linear interval of dataset
+    (Elstart, Elend), engr_slope, engr_intercept=find_best_interval(engr_strain,engr_stress)
+
+    engr_strain_subset = engr_strain[Elstart:Elend]
+    engr_stress_subset = engr_stress[Elstart:Elend]
+
+    engr_actual_offset = offset * (engr_strain_subset.max() - engr_strain_subset.min())
+    engr_offset_intercept = engr_intercept + engr_actual_offset
+
+    engr_range = np.linspace(0, engr_strain.max(), len(engr_stress))
+    engr_offset_line = engr_slope * engr_range + engr_offset_intercept
+
+    fig, ax = plt.subplots()
+    ax.plot(engr_strain, engr_stress, 'o-', label="Engineering Stress vs Strain")
+    ax.plot(engr_range, engr_offset_line, '--k', label="Offset Line")
+
+    ax.scatter(engr_strain[Elend-1],engr_stress[Elend-1], color='red',s=200)#yield
+
+    if engr_slope<0:
+        ax.text(0.02, 0.65, f"Invalid Data", transform=ax.transAxes, fontsize=12, color='red', ha='left', va='top')
+
+    ax.text(0.42, 0.78, f" Yield: {engr_strain[Elend-1]:.5f}, {engr_stress[Elend-1]:.5f} \n Young's Modulus: {engr_slope:.5f} \n Avg Temp: {avg_temperature:.2f}°C", transform=ax.transAxes, fontsize=10, verticalalignment='bottom', horizontalalignment='right', bbox=dict(facecolor='white', alpha=0.5))
+    ax.text(0.02, 0.98, f"Session ID: {session_id}", transform=ax.transAxes, fontsize=12, color='green', ha='left', va='top')
+    ax.set_xlabel("Strain")
+    ax.set_ylabel("Stress (Pa)")
+    ax.legend()
+
+    img_buf = io.BytesIO()
+    plt.savefig(img_buf, format='png')
+    img_buf.seek(0)
+    engr_stress_strain = base64.b64encode(img_buf.read()).decode('utf-8')
+    plt.close()
+
+    graphs_list.append(engr_stress_strain)
+
+    #------true stress and strain graph-------
+    true_strain=np.log(1+engr_strain)    
+    true_stress=engr_stress*(1+engr_strain)
+
+    (Tlstart, Tlend), true_slope, true_intercept=find_best_interval(true_strain,true_stress)
+
+    true_strain_subset = true_strain[Tlstart:Tlend]
+    true_stress_subset = true_stress[Tlstart:Tlend]
+
+    true_actual_offset = offset * (true_strain_subset.max() - true_strain_subset.min())
+    true_offset_intercept = true_intercept + true_actual_offset
+
+    true_range = np.linspace(0, true_strain.max(), len(true_stress))
+    true_offset_line = true_slope * true_range + true_offset_intercept
+    
+    fig, ax = plt.subplots()
+    ax.plot(true_strain, true_stress, 'o-', label="True Stress vs Strain")
+    ax.plot(true_range, true_offset_line, '--k', label="Offset Line")
+
+    ax.scatter(true_strain[Tlend-1],true_stress[Tlend-1], color='red',s=200)#yield
+    #ax.text(0.05, 0.90, f"Yield: {true_strain[Tlend-1]:.5f}, {true_stress[Tlend-1]:.5f}", transform=ax.transAxes, fontsize=12, color='purple', ha='left', va='top')
+    #ax.text(0.05, 0.85, f"Young's Modulus: {true_slope:.5f}", transform=ax.transAxes, fontsize=12, color='purple', ha='left', va='top')
+
+    if true_slope<0:
+        ax.text(0.02, 0.65, f"Invalid Data", transform=ax.transAxes, fontsize=12, color='red', ha='left', va='top')
+
+    ax.text(0.42, 0.78, f" Yield: {true_strain[Tlend-1]:.5f}, {true_stress[Tlend-1]:.5f} \n Young's Modulus: {true_slope:.5f} \n Avg Temp: {avg_temperature:.2f}°C", transform=ax.transAxes, fontsize=10, verticalalignment='bottom', horizontalalignment='right', bbox=dict(facecolor='white', alpha=0.5))
+    ax.text(0.02, 0.98, f"Session ID: {session_id}", transform=ax.transAxes, fontsize=12, color='green', ha='left', va='top')
+    ax.set_xlabel("True Strain")
+    ax.set_ylabel("True Stress (Pa)")
+    ax.legend()
+
+    img_buf = io.BytesIO()
+    plt.savefig(img_buf, format='png')
+    img_buf.seek(0)
+    true_stress_strain = base64.b64encode(img_buf.read()).decode('utf-8')
+    plt.close()
+
+    graphs_list.append(true_stress_strain)
+
     # --- Displacement vs Force Plot ---
     fig, ax = plt.subplots()
     ax.plot(displacements, force_N, 'o-', label="Displacement vs Force")
-    ax.text(0.95, 0.05, f"Avg Temp: {avg_temperature:.2f}°C", transform=ax.transAxes, fontsize=10, verticalalignment='bottom', horizontalalignment='right', bbox=dict(facecolor='white', alpha=0.5))
-    ax.text(0.05, 0.95, f"Session ID: {session_id}", transform=ax.transAxes, fontsize=12, color='green', ha='left', va='top')
+    ax.text(0.02, 0.98, f"Session ID: {session_id}", transform=ax.transAxes, fontsize=12, color='green', ha='left', va='top')
     ax.set_xlabel("Displacement (cm)")
     ax.set_ylabel("Force (N)")
     ax.legend()
@@ -146,48 +222,10 @@ def create_graphs(distances, weights, temperatures, timestamps, session_id, init
 
     graphs_list.append(displacement_force)
 
-    # --- Stress vs Strain Plot ---
-
-    #get linear interval of dataset
-    (lstart, lend), slope, intercept=find_best_interval(engr_strain,engr_stress)
-
-    strain_subset = engr_strain[lstart:lend]
-    stress_subset = engr_stress[lstart:lend]
-
-    actual_offset = offset * (strain_subset.max() - strain_subset.min())
-    offset_intercept = intercept + actual_offset
-
-    range = np.linspace(0, engr_strain.max(), len(engr_stress))
-    offset_line = slope * range + offset_intercept
-
-    fig, ax = plt.subplots()
-    ax.plot(engr_strain, engr_stress, 'o-', label="Stress vs Strain")
-    ax.plot(range, offset_line, '--k', label="Offset Line")
-
-    ax.scatter(engr_strain[lend-1],engr_stress[lend-1], color='red',s=200)#youngs modulus
-    ax.text(0.05, 0.90, f"Young's Modulus: {engr_strain[lend-1]:.5f}, {engr_stress[lend-1]:.5f}", transform=ax.transAxes, fontsize=12, color='purple', ha='left', va='top')
-
-    if slope<0:
-        ax.text(0.05, 0.85, f"Invalid Data", transform=ax.transAxes, fontsize=12, color='red', ha='left', va='top')
-
-    ax.text(0.95, 0.05, f"Avg Temp: {avg_temperature:.2f}°C", transform=ax.transAxes, fontsize=10, verticalalignment='bottom', horizontalalignment='right', bbox=dict(facecolor='white', alpha=0.5))
-    ax.text(0.05, 0.95, f"Session ID: {session_id}", transform=ax.transAxes, fontsize=12, color='green', ha='left', va='top')
-    ax.set_xlabel("Strain")
-    ax.set_ylabel("Stress (Pa)")
-    ax.legend()
-
-    img_buf = io.BytesIO()
-    plt.savefig(img_buf, format='png')
-    img_buf.seek(0)
-    stress_strain = base64.b64encode(img_buf.read()).decode('utf-8')
-    plt.close()
-
-    graphs_list.append(stress_strain)
-
     # --- Load vs Time Plot ---
     fig, ax = plt.subplots()
     ax.plot(time_seconds, force_N, 'o-', label="Load vs Time", color='g')
-    ax.text(0.05, 0.95, f"Session ID: {session_id}", transform=ax.transAxes, fontsize=12, color='green', ha='left', va='top')
+    ax.text(0.02, 0.98, f"Session ID: {session_id}", transform=ax.transAxes, fontsize=12, color='green', ha='left', va='top')
     ax.set_xlabel("Time (sec)")
     ax.set_ylabel("Force (N)")
     ax.legend()
@@ -203,7 +241,7 @@ def create_graphs(distances, weights, temperatures, timestamps, session_id, init
     # --- Displacement vs Time Plot ---
     fig, ax = plt.subplots()
     ax.plot(time_seconds, displacements, 'o-', label="Displacement vs Time", color='b')
-    ax.text(0.05, 0.95, f"Session ID: {session_id}", transform=ax.transAxes, fontsize=12, color='green', ha='left', va='top')
+    ax.text(0.02, 0.98, f"Session ID: {session_id}", transform=ax.transAxes, fontsize=12, color='green', ha='left', va='top')
     ax.set_xlabel("Time (sec)")
     ax.set_ylabel("Displacement (cm)")
     ax.legend()
@@ -215,5 +253,6 @@ def create_graphs(distances, weights, temperatures, timestamps, session_id, init
     plt.close()
 
     graphs_list.append(displacement_time)
+
 
     return graphs_list
