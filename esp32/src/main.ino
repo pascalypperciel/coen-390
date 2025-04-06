e41e6d07ced59dc4bcec03695f9342c31e24f1b3
Zatcheryy <zcohentravail@gmail.com>
2025-04-06 12:15:18 -0400

    rotary encoder code

--- esp32/src/main.ino
+++ esp32/src/main.ino
@@ -1,7 +1,8 @@
 #include <Wire.h>
 #include <Adafruit_MLX90614.h>
 #include "HX711.h"
-#include <HCSR04.h>
+//#include <HCSR04.h>
+#include "Rotary.h"
 #include <BluetoothSerial.h>
 #include <cmath>
 
@@ -10,8 +11,11 @@
 #define TEMP_I2C_SCL      22
 
 // Distance Sensor
-#define DIST_TRIG_PIN     13
-#define DIST_ECHO_PIN     12
+// #define DIST_TRIG_PIN     13
+// #define DIST_ECHO_PIN     12
+#define ROTARY_PIN1	26
+#define ROTARY_PIN2	27
+#define ROTARY_CM_PER_STEP 0.02692793703
 
 // Weight Sensor
 #define LOADCELL_DOUT_PIN 4
@@ -23,25 +27,40 @@
 
 #define BAUD    115200
 
-bool weight_cnt =true;
+bool weight_cnt = true;
 
 Adafruit_MLX90614 mlx = Adafruit_MLX90614();
 HX711 scale;
 BluetoothSerial Bluetooth;
+Rotary r = Rotary(ROTARY_PIN1, ROTARY_PIN2);
+
+volatile int rotaryPosition = 0;
+
+void updateRotaryPosition(Rotary& r) {
+  rotaryPosition = r.getPosition();
+}
 
 void TaskBluetooth(void *pvParameters) {
   while (1) {
-    // Distance Sensor
-    double* distances = HCSR04.measureDistanceCm(); // in cm
-    float distance = (distances != nullptr) ? distances[0] : NAN;
-    distance=10.55-distance;
-
-    // Temperature Sensor
-    float temperature = mlx.readObjectTempC(); // in C
-
     // Weight Sensor
     float weight = scale.get_units(5); // averages 5 readings, tweak with it. In g.
     weight = (weight < 5) ? NAN : weight;
+
+    // Distance Sensor
+    // double* distances = HCSR04.measureDistanceCm(); // in cm
+    // float distance = (distances != nullptr) ? distances[0] : NAN;
+    // distance=10.55-distance;
+    r.loop();
+    float distance = 0;
+    if (!isnan(weight)) {
+      distance = rotaryPosition * 0.02692793703; // cm
+    } else {
+      rotaryPosition = 0; // Reset position if no object
+      r.setPosition(0);
+    }
+
+    // Temperature Sensor
+    float temperature = mlx.readObjectTempC(); // in C
 
     // Format message in standardized format
     char message[128];
@@ -97,9 +116,13 @@ void setup() {
   scale.tare();
 
   // Distance Sensor
-  HCSR04.begin(DIST_TRIG_PIN, DIST_ECHO_PIN);
+  // HCSR04.begin(DIST_TRIG_PIN, DIST_ECHO_PIN);
 
-  //Motor Controller
+  // Rotary Encoder Setup
+  r.setPosition(0); // Reset rotary position on startup
+  r.setChangedHandler(updateRotaryPosition);
+
+  // Motor Controller
   pinMode(FWD, OUTPUT);
   digitalWrite(FWD, LOW);
   pinMode(BWD, OUTPUT);
@@ -111,5 +134,4 @@ void setup() {
 }
 
 void loop() {
-  //unneeded
 }
